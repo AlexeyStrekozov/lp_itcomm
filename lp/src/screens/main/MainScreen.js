dd39e32e34dd40aeee5527262773f2868baeb546
strekozovalexey <strekozovalexey@gmail.com>
2023-08-21 16:55:41 +0600

    Main

---
+++ lp/src/screens/main/MainScreen.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './image/logo.png';
+import Button from '../../components/Button/Button.js';
+import './style.css';
+
+class MainScreen extends Component {
+    render() {
+        return <section className="Main">
+            <header className="Main-header container">
+                <img src={logo} />
+                <nav className='Main-nav'>
+                    <a href="#" className='Main_nav_item button_text'>О нас</a>
+                    <a href="#" className='Main_nav_item button_text'>Услуги</a>
+                    <a href="#" className='Main_nav_item button_text'>Реализованные проекты</a>
+                    <a href="#" className='Main_nav_item button_text'>Рекомендации</a>
+                    <a href="#" className='Main_nav_item button_text'>Контакты</a>
+                </nav>
+            </header>
+            <div className='container'>
+                <div className='Main_title title'>
+                    УСЛУГИ<br />
+                    ТЕЛЕКОММУНИКАЦИЙ<br />
+                    И СИСТЕМНОЙ ИНТЕГРАЦИИ
+                </div>
+                <Button title='IT-COMM' />
+            </div>
+        </section>
+
+    }
+}
+
+export default MainScreen;
