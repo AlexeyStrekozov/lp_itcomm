991c6bf8df2a3a83abc0a9cf94d7629ede679945
strekozovalexey <strekozovalexey@gmail.com>
2023-08-25 15:02:43 +0600

    new adaptive

--- lp/src/screens/letters_recommendation/letters_recommendation.jsx
+++ lp/src/screens/letters_recommendation/letters_recommendation.jsx
@@ -48,13 +48,13 @@ export default function LettersRecommendation() {
                 </Slider>
                 <div className='LettersRecommendation-controls'>
                     <button onClick={sliderRef?.slickPrev}>
-                        <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1.25 9.29904C0.250001 8.72169 0.25 7.27831 1.25 6.70096L11.75 0.638783C12.75 0.0614319 14 0.783119 14 1.93782L14 14.0622C14 15.2169 12.75 15.9386 11.75 15.3612L1.25 9.29904Z" fill="#AB0B0A" />
                         </svg>
 
                     </button>
                     <button onClick={sliderRef?.slickNext}>
-                        <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12.75 9.29904C13.75 8.72169 13.75 7.27831 12.75 6.70096L2.25 0.638783C1.25 0.0614319 0 0.783119 0 1.93782L0 14.0622C0 15.2169 1.25 15.9386 2.25 15.3612L12.75 9.29904Z" fill="#AB0B0A" />
                         </svg>
 
